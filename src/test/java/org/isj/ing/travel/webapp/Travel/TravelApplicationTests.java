4663e9b67714cc9542f89f8f41329ed74c8cd95e
fleuronne <vinny.ngaleu@institutsaintjean.org>
2022-05-26 19:05:42 +0200

    ajout des dependances spring

---
+++ src/test/java/org/isj/ing/travel/webapp/Travel/TravelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.isj.ing.travel.webapp.Travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
